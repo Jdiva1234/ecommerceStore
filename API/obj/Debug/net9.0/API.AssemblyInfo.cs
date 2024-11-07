fc12da8cab427978efc5b28e9710846da5ae7914
Justice <unankajustice@gmail.com>
2024-11-07 21:40:50 +0000

    creating an entity migration

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40bf7d0838e32c57a394b6ea2f27b1f87e68a1f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a546328d01cf1571d2e5c305839240b3663e7fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
