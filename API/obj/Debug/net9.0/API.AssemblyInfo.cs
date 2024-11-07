40bf7d0838e32c57a394b6ea2f27b1f87e68a1f4
Justice <unankajustice@gmail.com>
2024-11-07 15:14:02 +0000

    Disable nullable reference types and fix syntax error in Product.cs

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72ba0f055cabaa9e523e9a9838742328a006fdc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5bcba9ac96454da9991ae9ab76b7d9bba50a675")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
